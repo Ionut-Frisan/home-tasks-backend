1a9ea17b0673a1ee7bd9253677afcabff2d5f550
Ionut Frisan <64963028+Ionut-Frisan@users.noreply.github.com>
2022-08-30 22:39:35 +0300

    Update db.js

--- db.js
+++ db.js
@@ -11,8 +11,7 @@ export const initDb = () => {
   db.read();
   if(!db.data['tasks']) db.data['tasks'] = []
   if(!db.data['calendar']) db.data['calendar'] = []
-  db.write()
-  console.log(db.data);
+  db.write();
 }
 
 export default db;
